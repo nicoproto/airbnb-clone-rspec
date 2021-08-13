89e60bb288d0fcf977fdaa2bdf1e40f02d66bbd8
Nico Proto <nicolasproto@gmail.com>
2021-08-13 19:53:43 +0200

    DOCS: Removed comment

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,7 +12,6 @@ describe User, type: :model do
 
   describe '#full_name' do
     it 'returns the full_name for a user' do
-      # user = User.create(first_name: "Chipper", last_name: "Roe")
       user = build(:user, first_name: "Chipper", last_name: "Roe")
 
       expect(user.full_name).to eq "Chipper Roe"
