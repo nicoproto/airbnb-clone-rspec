e4d98ffd7c900ea8ebd664585d3765dcda5f5d8f
Nico Proto <nicolasproto@gmail.com>
2021-08-13 19:52:22 +0200

    FEAT: Added :first_name and :last_name fields to users and #full_name to user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
   add_flash_types :info, :success
 
   include Pundit
@@ -15,9 +16,21 @@ class ApplicationController < ActionController::Base
   #   redirect_to(root_path)
   # end
 
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+  end
+
   private
 
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
 end
+
+
+
+
